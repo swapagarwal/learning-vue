f361faf6024491db328a192519cf199501b98210
Swapnil Agarwal <swapnilagarwal@live.com>
2017-11-24 10:39:38 +0530

    My first Vue Component!

--- testing-vue/test/counter.spec.js
+++ testing-vue/test/counter.spec.js
@@ -1,5 +1,5 @@
 import { mount } from 'vue-test-utils';
-import Counter from '../src/components/Counter.js';
+import Counter from '../src/components/Counter.vue';
 import expect from 'expect';
 
 describe ('Counter', () => {
